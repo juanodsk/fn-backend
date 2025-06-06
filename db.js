d73bd120b7533e8c7fbb8e8b732bac214b0a69fe
juanodsk <juanph.dv@gmail.com>
2025-06-05 21:20:22 -0500

    cambios db.js

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
-dotenv.config();
 
-export const connectDB = async () => {
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Base de datos conectada");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("Error al conectar a la base de datos:", error);
+    process.exit(1);
   }
 };
+
+export default connectDB;
